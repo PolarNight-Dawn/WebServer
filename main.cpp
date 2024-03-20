a6f714b1ae4f58be5afc497e90b927ece3994e1e
polarnight <1813959165@qq.com>
2024-03-20 20:59:39 +0800

    修改处理非活动连接 bug

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include <sys/epoll.h>
 #include <cerrno>
 #include <unistd.h>
+#include <sys/syslog.h>
 
 #include "locker.h"
 #include "threadpool.h"
@@ -23,6 +24,14 @@
 
 #define MAX_FD 65536
 #define MAX_EVENT_NUMBER 10000
+#define TIMESLOT 5
+
+static int pipefd[2];
+static sort_timer_lst timer_list;
+static int epollfd = 0;
+
+/* 设置非阻塞 */
+extern int setnonblocking(int fd);
 
 /* 注册事件到 epoll 指定的内核事件表 */
 extern void addfd(int epollfd, int fd, bool one_shot);
@@ -42,6 +51,24 @@ void addsig(int sig, void(*sig_handler)(int), bool restart = true) {
     }
     sigfillset(&sa.sa_mask);
     assert(sigaction(sig, &sa, nullptr) != -1);
+}
+
+void sig_handler(int sig) {
+    int save_errno = errno;
+    int msg = sig;
+    send(pipefd[1], (char *) &msg, 1, 0);
+    errno = save_errno;
+}
+
+void timer_handler() {
+    timer_list.tick();
+    alarm(TIMESLOT);
+}
+
+void cb_func(client_data *user_data) {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
+    assert(user_data);
+    close(user_data->sockfd);
 }
 
 void show_errno(int connfd, const char *info) {
@@ -101,7 +128,7 @@ int main(int argc, char *argv[]) {
 
     /* 创建 epoll 实例 */
     epoll_event events[MAX_EVENT_NUMBER];
-    int epollfd = epoll_create(5);
+    epollfd = epoll_create(5);
     assert(epollfd != -1);
     addfd(epollfd, listenfd, false);
     http_conn::m_epollfd = epollfd;
@@ -109,39 +136,24 @@ int main(int argc, char *argv[]) {
     /* 创建管道套接字 */
     ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
     assert(ret != -1);
-
     /* 写端设置非阻塞 */
     setnonblocking(pipefd[1]);
-
     /* 设置管道读端为 ET 非阻塞 */
     addfd(epollfd, pipefd[0], false);
 
     /* 传递给主循环的信号 */
     addsig(SIGALRM, sig_handler, false);
     addsig(SIGTERM, sig_handler, false);
-
-    /* 循环条件 */
     bool stop_server = false;
-
-    /* 定时处理任务，重新定时以不断触发 SIGALRM 信号 */
-    void timer_handler{
-        timer_lst.tick();
-        alarm(TIMESLOT);
-    };
-
-    /* 创建定时器容器链表 */
-    static sort_timer_lst timer_list;
 
     /* 创建连接资源数组 */
     client_data *user_timer = new client_data[MAX_FD];
-
     /* 超时标志 */
     bool timeout = false;
-
     /* 每隔TIMESLOT 时间触发SIGALRM信号 */
     alarm(TIMESLOT);
 
-    while (true) {
+    while (!stop_server) {
         /* 就绪事件的数量 */
         int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
         if ((number < 0) && (errno != EINTR)) {
@@ -168,6 +180,13 @@ int main(int argc, char *argv[]) {
                 }
                 /* 初始化客户连接 */
                 users[connfd].init(connfd, client_address);
+                util_timer *timer = new util_timer;
+                timer->user_data = &users[connd];
+                timer->cb_func = cb_func;
+                time_t  cur = time(nullptr);
+                timer->expire = cur + 3 * TIMESLOT;
+                users[connd].timer = timer;
+                timer_list.add_timer(timer);
 #endif
 
 #ifdef ET
@@ -183,18 +202,64 @@ int main(int argc, char *argv[]) {
                     }
                     /* 初始化客户连接 */
                     users[connfd].init(connfd, client_address);
+                    util_timer *timer = new util_timer;
+                    timer->user_data = &users[connd];
+                    timer->cb_func = cb_func;
+                    time_t cur = time(nullptr);
+                    timer->expire = cur + 3 * TIMESLOT;
+                    users[connd].timer = timer;
+                    timer_list.add_timer(timer);
                 }
                 continue;
 #endif
+            } else if ((sockfd == pipefd[0] && (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)))) {
+                int sig;
+                char signals[1024];
+                ret = recv(pipefd[0], signals, sizeof(signals), 0);
+                if (ret == -1) {
+                    continue;
+                } else if (ret == 0) {
+                    continue;
+                } else {
+                    for (int i = 0; i < ret; i++) {
+                        switch (signals[i]) {
+                            case SIGALRM: {
+                                timeout = true;
+                                break;
+                            }
+                            case SIGTERM: {
+                                stop_server = true;
+                            }
+                        }
+                    }
+                }
             } else if (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
                 /* 发生异常，直接关闭客户连接 */
                 users[sockfd].close_conn();
+
+                cb_func(&user_timer[sockfd]);
+                util_timer *timer = user_timer[sockfd].timer;
+                if (timer) {
+                    timer_list.del_timer(timer);
+                }
             } else if (events[i].events & EPOLLIN) {
+                util_timer *timer = user_timer[sockfd].timer;
                 /* 根据读的结果，决定是将任务添加到线程池，还是关闭连接 */
                 if (users[sockfd].read()) {
                     pool->append(users + sockfd);
+
+                    if (timer) {
+                        time_t cur = time( NULL );
+                        timer->expire = cur + 3 * TIMESLOT;
+                        printf( "adjust timer once\n" );
+                        timer_list.adjust_timer( timer );
+                    }
                 } else {
                     users[sockfd].close_conn();
+                    cb_func(&user_timer[sockfd]);
+                    if (timer) {
+                        timer_list.del_timer(timer);
+                    }
                 }
             } else if (events[i].events & EPOLLOUT) {
                 /* 根据写的结果，决定是否关闭连接*/
@@ -202,11 +267,18 @@ int main(int argc, char *argv[]) {
                     users[sockfd].close_conn();
                 }
             }
+            if (timeout) {
+                timer_handler();
+                timeout = false;
+            }
         }
     }
 
     close(epollfd);
     close(listenfd);
+    close(pipefd[1]);
+    close(pipefd[0]);
+    delete[] user_timer;
     delete[] users;
     delete pool;
 
